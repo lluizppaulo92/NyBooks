12fab41233d9093ef51fd5925a035e4e73e7c85e
LuizPaulo <luiz_1.8vs@hotmail.com>
2020-01-27 09:57:35 -0300

    commit inicial do projeto

---
+++ app/src/main/java/br/com/lluizppaulo/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package br.com.lluizppaulo.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.lluizppaulo.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
